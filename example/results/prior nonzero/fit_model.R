022de32c739d4b682aee6495a375669bd2c758fd
Berkhout <s.w.berkhout@uu.nl>
2024-05-10 10:26:24 +0200

    clean results priors

--- example/results/prior nonzero/fit_model.R
+++ example/results/prior nonzero/fit_model.R
@@ -2,21 +2,27 @@ source("example/results/functions_results.R")
 
 dat <- read.csv("example/data/data.csv")
 
+# only items of interest
 items <- names(dat)[grepl("mood_|pat_|phy_|se_", names(dat))]
 
+# parallelize
 n_threads <- length(items)
 clus <- parallel::makeCluster(n_threads)
 exp <- parallel::clusterExport(clus, "items")
 
+# compile Stan model
 # mean of gamma in main result is 0.29
 stan_code <- getStanCode(prior_gamma = "normal(0.3, sqrt(0.5))")
 mod <- rstan::stan_model(model_code = stan_code)
 
+# put data in right format
 dat_stan <-  parallel::parLapplyLB(cl = clus, 1:length(items),
                                    stanData, dat, items)
 
+# save pars
 pars <- c("mu", "phi", "gamma", "sigma_2", "psi_2", "diff_phi", "diff_phi_ct")
 
+# fit model
 modelout <- "example/results/prior nonzero/out/"
 t_fit <- system.time(
   parallel::parLapplyLB(cl = clus, 1:length(items), fitModel,
--- example/results/prior wide/read_results.R
+++ example/results/prior wide/read_results.R
@@ -1,27 +1,35 @@
 source("example/results/functions_results.R")
 
+# prior for bf computation
 prior <- c(0, 2.5)
 
+# get item names
 dat <- read.csv("example/data/data.csv")
-
 items <- names(dat)[grepl("mood_|pat_|phy_|se_", names(dat))]
 
+# read results
 out <- lapply(1:length(items), readStanResults,
               modelout = "example/results/prior wide/out/", items = items)
 
+# pars of interest
 pars <- c("mu", "phi", "gamma", "sigma_2", "psi_2", "diff_phi", "diff_phi_ct")
+
+# put results in nice format
 res <- lapply(1:length(items), getResults, out = out, pars = pars)
 names(res) <- prettyNames()
 
+# compute Savage-Dickey density ratio and posterior model probabilities
 logSpline <- lapply(1:length(items), fitLogSpline, out)
 posterior_density <- lapply(1:length(items), getDensity, logSpline)
 bfs  <- lapply(1:length(items), getBF, out, prior, logSpline)
 pmps <- lapply(1:length(items), calcPostModProbs, bfs)
 
+# put results in data frames for plotting
 df_pars <- dfPars(res)
 df_posteriors <- dfDensity(posterior_density, prior, res)
 df_bayes_factors <- dfBayesFactors(bfs)
 df_pmps <- dfPMPs(pmps)
 
+# save dfs
 save(df_pars, df_posteriors, df_bayes_factors, df_pmps,
      file = "example/results/prior wide/objects.Rdata")
